634d90cd6b7f64e6f79d0dac8af2383a7f119f0d
Ananda Bayu <anandabayuf@fakfak.com>
2019-05-10 21:24:03 +0700

    menambahkan beberapa abstract method yang mungkin dibutuhkan untuk class yang mengimplementasikan nya

---
+++ ATM_D4/src/com/appl/atm/model/AccountInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appl.atm.model;
+
+/**
+ *
+ * @author Ananda Bayu
+ */
+public interface AccountInterface {
+    public abstract void getAccountNumber();
+    public abstract void setAccountNumber(int accountNumber);
+    public abstract void getPin();
+    public abstract void setPin(int pin);
+    public abstract void getAvailableBalance();
+    public abstract void setAvailableBalance(double availableBalance);
+    public abstract void getTotalBalance();
+    public abstract void setTotalBalance(double totalBalance);
+    
+}
